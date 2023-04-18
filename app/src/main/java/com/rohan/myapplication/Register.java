dddd10ba7070d1716e563e8e99674879e07f989e
Rohan Napit <rohan.napit@gmail.com>
2023-04-18 07:29:30 +0545

    my 1st

--- app/src/main/java/com/rohan/myapplication/Register.java
+++ app/src/main/java/com/rohan/myapplication/Register.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity {
     Button back;
-
+    RadioGroup btnGrp;
+    RadioButton male;
+    RadioButton female;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +24,34 @@ public class Register extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent=new Intent(Register.this, Login_Activity.class);
                 startActivity(intent);
-
+            }
+        });
+        btnGrp=(RadioGroup)findViewById(R.id.genderRadio);
+        male=(RadioButton)findViewById(R.id.male);
+        female=(RadioButton)findViewById(R.id.female);
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onRadioButtonClicked(view);
+            }
+        });
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onRadioButtonClicked(view);
             }
         });
     }
+    public void onRadioButtonClicked(View view){
+        boolean checked=((RadioButton)view).isChecked();
+        switch (view.getId()){
+            case R.id.male:
+                if(checked)
+                    break;
+            case R.id.female:
+                if(checked)
+                    break;
+        }
+    }
+
 }
