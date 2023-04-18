c7270840f83f1658938f811a39039b906c3e6de6
Rohan Napit <rohan.napit@gmail.com>
2023-04-18 07:37:18 +0545

    my 1st

--- app/src/main/java/com/rohan/myapplication/ListActivity.java
+++ app/src/main/java/com/rohan/myapplication/ListActivity.java
@@ -1,14 +1,28 @@
-package com.rohan.myapplication;
+package app.mobile.asianpay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.rohan.myapplication.R;
 
 public class ListActivity extends AppCompatActivity {
 
+    ListView myListView;
+    String[] dataArray = {"One","Two","Three","Four","Five","Six","Seven","Eight", "One","Two","Three","Four","Five","Six","Seven", "One","Two","Three","Four","Five","Six","Seven", "One","Two","Three","Four","Five","Six","Seven", "One","Two","Three","Four","Five","Six","Seven","Eight", "One","Two","Three","Four","Five","Six","Seven", "One","Two","Three"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        myListView = findViewById(R.id.list_view);
+
+        /*ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_item, dataArray);*/
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_item, dataArray);
+        myListView.setAdapter(adapter);
+
+
     }
 }
